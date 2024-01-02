2302fcfdfb3a380d2d839bef8964dbadffe0f1be
JISOO <jisoo9413@gmail.com>
2024-01-02 16:54:00 +0900

    20240102

--- partA/src/basic/day3/A15MinValue copy.java
+++ partA/src/basic/day3/A15MinValue copy.java
@@ -1,6 +1,6 @@
 package basic.day3;
 
-public class A15MinValue { //가장 작은 값 찾기
+public class A15MinValuecopy { //가장 작은 값 찾기
     public static void main(String[] args) {
         int n1 = 23;
         int n2 = 24;
@@ -36,4 +36,32 @@ public class A15MinValue { //가장 작은 값 찾기
 
 }
 
+    @Override
+    public String toString() {
+        return "A15MinValue []";
+    }
+
 }
+/* 
+* ### 순서
+* 한번만 할 것들
+* git init
+* git remote add origin
+* git config --global user.email
+* git config --global user.name
+
+* 반복되는 작업은
+* 1. vs code : staged Changes 로 커밋할 파일 선택하기
+* 2. vs code : commin 하기. 메시지 꼭 작성하기
+* 3. vs code : 메뉴에서 push. githug으로 소스 전송 확인하세요.
+
+* 집에가서 한번만 할 것
+* 1. git clone 원격주소
+* 2. git config --global user.email
+* 3. git config --global user.name
+* 4. clone 으로 생긴 프로젝트를 bs clone 열기
+
+* 집에가서 반복할 작업은
+* 1. vs code : 메뉴에서 pull. githug에 있는 소스를 받아오기(동기화)
+* 2. 새로 작성한 연습 코드 소스 파일은 stage - commit - push
+*/
--- partA/src/basic/day3/A20BankExam.java
+++ partA/src/basic/day3/A20BankExam.java
@@ -12,54 +12,50 @@ public class A20BankExam {
     boolean run=true;
 
     Scanner sc = new Scanner(System.in);
-    sc.nextInt();
-    System.out.println(" ");
 
     //세자리마다 컴마 찍기 : 아직 배운 진도가 아니지만 일단은 사용하기
-      DecimalFormat df = new DecimalFormat("#,### 원");
+    DecimalFormat df = new DecimalFormat("금 #,### 원");
     while (run) {
         System.out.println("1:예금 2:출금 3:잔액 4:종료");
         System.out.print("메뉴선택. >>>>");
-
+        menu =sc.nextInt();
         switch(menu) {
             case 1:
               System.out.print("예금액 >");
               money = sc.nextInt();
               balance += money;
-              System.out.println("고객님의 잔액은" +df.format(balance) + "원 입니다");
+              System.out.println("고객님의 잔액은 " +df.format(balance) + " 입니다");
                 break;
 
             case 2:
               System.out.print("출금액 >");
               money = sc.nextInt();
-              if(money <=balance)
+              if(money <=balance){
                 balance -= money;
-              else
+                System.out.println("고객님의 잔액은 " +df.format(balance) + " 입니다");
+              }else{
                 System.out.println("고객님의 잔액이 출금액보다 부족합니다.");
-              System.out.println("고객님의 잔액은" +df.format(balance) + "원 입니다");
+                System.out.println("고객님의 잔액은 " +df.format(balance) + " 입니다");
+              }
               break;
 
             case 3:
-              System.out.print("현재잔고 : "+df.format(balance)+ "원 입니다");
+              System.out.println("현재잔고 : "+df.format(balance)+ " 입니다");
                 break;
 
             case 4:
-    
-              System.out.print("종료");
+              System.out.print("종료합니다.");
                 break; 
 
             default:
               System.out.print("잘못된 입력값입니다. 1~4만 선택하세요");
                 break;
+            }
+
+
         }
 
-
     }
-
-
-
-
-}
 
 
 }
