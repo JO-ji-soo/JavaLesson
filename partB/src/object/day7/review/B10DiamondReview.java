d2b7624126013d3037a26d014df6e148f5613f1d
jisoo <https://github.com/JO-ji-soo/JavaLesson.git>
2024-01-08 17:18:39 +0900

    20240108

--- partB/src/object/day7/review/B10DiamondReview.java
+++ partB/src/object/day7/review/B10DiamondReview.java
@@ -1,5 +1,12 @@
 package object.day7.review;
 
+import object.day7.Diamond;
+
 public class B10DiamondReview {
-  
-}
+ public static void main(String[] args) {
+  Diamond diamond = new Diamond();
+  System.out.println("다이아몬드 너비 :"+diamond.getWidth());
+  diamond.print();
+
+
+}}
